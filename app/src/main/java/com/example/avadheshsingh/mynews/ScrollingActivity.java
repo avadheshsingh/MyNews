d1bcb1b22b955024515f4e9cc86f333244c1c6a7
Avadhesh Singh <avadheshsingh151@gmail.com>
2016-12-31 17:39:00 +0530

    uploaded

---
+++ app/src/main/java/com/example/avadheshsingh/mynews/ScrollingActivity.java
@@ -0,0 +1,38 @@
+package com.example.avadheshsingh.mynews;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        SliderLayout sliderShow = (SliderLayout) findViewById(R.id.slider);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        TextSliderView textSliderView = new TextSliderView(this);
+        textSliderView
+                .description("Game of Thrones")
+                .image("http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        sliderShow.addSlider(textSliderView);
+    }
+}
